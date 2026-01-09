5ef588d8434e70c2fa4b302feeb0eaa8995ab250
Arpit Sharma <100201319+arpitsharma54@users.noreply.github.com>
2026-01-10 03:13:26 +0530

    Update iob_bot.py

--- core/bot/iob_bot/iob_bot.py
+++ core/bot/iob_bot/iob_bot.py
@@ -764,7 +764,7 @@ async def run_bot_for_account(bank_account_id: int):
 
         async with async_playwright() as p:
             browser = await p.chromium.launch(
-                headless=True,
+                headless=False,
                 args=[
                     "--no-sandbox",
                     "--disable-setuid-sandbox",
